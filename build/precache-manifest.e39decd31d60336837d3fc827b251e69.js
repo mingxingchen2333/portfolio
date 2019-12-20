377440a88101b47674b09680b0c31ba74201207a
Xingchen Ming <xingchenming@xingchens-mbp.devices.brown.edu>
2019-12-20 17:36:58 -0500

    add #

---
+++ build/precache-manifest.e39decd31d60336837d3fc827b251e69.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e80ac7549ef94c9dc9b2afb8e7eca59",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "768d9cbc7120ce316c13",
+    "url": "/portfolio/static/css/main.de8e709c.chunk.css"
+  },
+  {
+    "revision": "2ebe93f6f1bc5f457c70",
+    "url": "/portfolio/static/js/2.904e492a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.904e492a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "768d9cbc7120ce316c13",
+    "url": "/portfolio/static/js/main.144df231.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "aa771deab9922917adff389f65ea0393",
+    "url": "/portfolio/static/media/HomePage.aa771dea.png"
+  },
+  {
+    "revision": "8a6279574f769c7d2e0bf39c06ee0489",
+    "url": "/portfolio/static/media/New.8a627957.mp4"
+  },
+  {
+    "revision": "b2626ab2712a611628f06081dce62f48",
+    "url": "/portfolio/static/media/NewProject2333.b2626ab2.mp4"
+  },
+  {
+    "revision": "87961f78340b388f4a554e4902b9cc8d",
+    "url": "/portfolio/static/media/apps.87961f78.png"
+  },
+  {
+    "revision": "3a236f3edee7ed793a3c38b9b6a6919e",
+    "url": "/portfolio/static/media/choose.3a236f3e.mp4"
+  },
+  {
+    "revision": "0722690e91123c93883b6445915fe8b4",
+    "url": "/portfolio/static/media/filterVd.0722690e.mp4"
+  },
+  {
+    "revision": "dbcf9652115bdf6755eaabb07a26b16d",
+    "url": "/portfolio/static/media/hello.dbcf9652.png"
+  },
+  {
+    "revision": "af945c46651ae6c03343e44cc7c29c20",
+    "url": "/portfolio/static/media/home.af945c46.png"
+  },
+  {
+    "revision": "872f35d7efa66af93a9e57e7f9d9b500",
+    "url": "/portfolio/static/media/kraftful.872f35d7.png"
+  },
+  {
+    "revision": "296c0d5990755ccd46948a79eb5436aa",
+    "url": "/portfolio/static/media/login.296c0d59.mp4"
+  },
+  {
+    "revision": "5c231876338306c580fb7652a855aaab",
+    "url": "/portfolio/static/media/miui.5c231876.png"
+  },
+  {
+    "revision": "4044f1ea477a0c6db89187786a1a05b6",
+    "url": "/portfolio/static/media/mode.4044f1ea.mp4"
+  },
+  {
+    "revision": "4c8ea1bd38d6ff82a74a51390f6937aa",
+    "url": "/portfolio/static/media/music.4c8ea1bd.png"
+  },
+  {
+    "revision": "f7b8081201ea140526c62e035458a12a",
+    "url": "/portfolio/static/media/poster.f7b80812.png"
+  },
+  {
+    "revision": "e269b19a0e57df485b27222eb6726768",
+    "url": "/portfolio/static/media/redesign.e269b19a.png"
+  },
+  {
+    "revision": "0f1d7750c813bc15e450648c60600a58",
+    "url": "/portfolio/static/media/signIn.0f1d7750.png"
+  },
+  {
+    "revision": "4313a533c37913d1233bf13e55973545",
+    "url": "/portfolio/static/media/start.4313a533.png"
+  }
+]);
